25c4d07cd73d31d9bc3466e54477ed5cad902a6e
BabayanRA <babayanroman@yandex.ru>
2022-03-03 15:37:32 +0400

    1-1 Finish

---
+++ 1-1.py
@@ -0,0 +1,13 @@
+c1 = float(input("Введите первое число:"))
+c2 = float(input("Введите второе число:"))
+d = input("Введите знак:")
+if d == "*":
+    print(c1 * c2)
+elif d == "+":
+    print(c1 + c2)
+elif d == "-":
+    print(c1 - c2)
+elif d == "/":
+    print(c1 / c2)
+elif d == "^":
+    print(c1 ** c2)
