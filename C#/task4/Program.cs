f2951457ccd7d3211c131f7801875011ecdaefc4
Babayan_RA <babayanroman@yandex.ru>
2023-04-24 17:52:00 +0400

    Задача 4 завершена

---
+++ C#/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+using System;
+
+Console.Write("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int[] a=new int[num+1];
+for(int i=1;i<=num;i++)
+{
+    a[i] = i;
+    if (a[i] % 2==0)
+    {
+        Console.Write($"{a[i]} ");
+    }
+
+}
