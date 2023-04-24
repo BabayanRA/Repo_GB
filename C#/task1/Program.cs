f30aa3ba053e43d118f8ff3babd709e2f95a7e2f
Babayan_RA <babayanroman@yandex.ru>
2023-04-24 16:03:01 +0400

    Создана папка C# и папи для 1-ого ДЗ

---
+++ C#/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+using System;
+
+Console.Write("Введите 1-ое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите 2-ое число:");
+int num2 = int.Parse(Console.ReadLine());
+int min = Math.Min(num1,num2);
+int max = Math.Max(num1,num2);
+Console.Write("Max=", max);
+Console.Write("Min=", min);
