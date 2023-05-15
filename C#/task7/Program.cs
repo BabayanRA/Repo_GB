40bf74793021620dca477eff83f7b8b29c9a6df9
Babayan_RA <babayanroman@yandex.ru>
2023-05-15 18:26:07 +0400

    task7 complete

---
+++ C#/task7/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int day)
+{
+    if (day > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+bool ValidateDay(int number)
+{
+    if (number > 0 && number <=7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели");
+    return false;
+}
+
+int day = Prompt ("Введите день недели:");
+if (ValidateDay(day))
+{
+    if (IsWeekend(day))
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else
+    {
+        Console.WriteLine("Это будний день");
+    }
+}
