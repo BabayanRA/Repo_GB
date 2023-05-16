4b534860b29f705dab04226b439717eea1b60349
Babayan_RA <babayanroman@yandex.ru>
2023-05-16 10:40:18 +0400

    task8 complete

--- C#/task8_3dz/Program.cs
+++ C#/task8_3dz/Program.cs
@@ -8,3 +8,23 @@
 23432 -> да
 */
 
+bool IsPalindrom(int num)
+{
+    if ((num / 10000 == num % 10) && (num / 1000 % 10 == num / 10 % 10))
+    {
+        Console.WriteLine("Это палиндром");
+        return true;
+    }
+
+    Console.WriteLine("Это не палиндром");
+    return false;
+}
+
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine());
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Это не пятизначное число:");
+}
+IsPalindrom(number);
