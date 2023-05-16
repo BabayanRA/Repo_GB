6267c403403fd322bee163dfe8b9a820988b94cd
Babayan_RA <babayanroman@yandex.ru>
2023-05-16 11:24:03 +0400

    task10 complete

--- C#/task10_3dz/Program.cs
+++ C#/task10_3dz/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+using System;
+
+Console.Write("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int[] a=new int[num+1];
+for(int i=1;i<=num;i++)
+{
+    a[i] = i;
+    Console.Write($"{Math.Pow(a[i],3)} ");
+}
